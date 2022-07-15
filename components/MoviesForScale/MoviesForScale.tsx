1a514efdd9b9effd5f1739b16d1ee143a5269532
Kazerlelutin <b@bouteiller.contact>
2022-07-16 00:31:41 +0200

    fix(movies-for-scale): level empty hidden

--- components/MoviesForScale/MoviesForScale.tsx
+++ components/MoviesForScale/MoviesForScale.tsx
@@ -34,35 +34,46 @@ export default function MoviesForScale({
     ),
     { loading, data } = useFetch("/scale/movies/", { id: scaleId }),
     [currentMovie, setCurrentMovie] = useState<Movie | null>(null);
-
+    
   useEffect(() => {
     if (data) createLevel(data);
   }, [data]);
   return (
     <div className={classes.container}>
-      {currentMovie && <MoviePopin movie={currentMovie} onClose={()=>setCurrentMovie(null)} /> }
+      {currentMovie && (
+        <MoviePopin
+          movie={currentMovie}
+          onClose={() => setCurrentMovie(null)}
+        />
+      )}
       <ToastLoading loading={loading} text={"chargement de l'echelle"} />
       {isMyScale && levels.length === 0 && "Ajoute ton premier film"}
       {!isMyScale && levels.length === 0 && "Aucun film sur cette échelle"}
       <div className={classes.container}>
-        {levels.map((level) => (
-          <Level
-            key={level.position}
-            level={level}
-            addMovie={addMovie}
-            scaleId={scaleId}
-            ownerId={ownerId}
-          >
-            {level.movies.map((movie: Movie) => (
-              <div key={movie.id} onClick={() => setCurrentMovie(movie)}>
-                <MovieToDrag movie={movie} levels={levels} isMyScale={isMyScale} />
-              </div>
-            ))}
-            {isMyScale && levels.length > 0 && (
-              <AddMedia onClick={() => openPopin(level.position)} />
-            )}
-          </Level>
-        ))}
+        {levels
+          .filter((o) => isMyScale || o.movies.length > 0)
+          .map((level) => (
+            <Level
+              key={level.position}
+              level={level}
+              addMovie={addMovie}
+              scaleId={scaleId}
+              ownerId={ownerId}
+            >
+              {level.movies.map((movie: Movie) => (
+                <div key={movie.id} onClick={() => setCurrentMovie(movie)}>
+                  <MovieToDrag
+                    movie={movie}
+                    levels={levels}
+                    isMyScale={isMyScale}
+                  />
+                </div>
+              ))}
+              {isMyScale && levels.length > 0 && (
+                <AddMedia onClick={() => openPopin(level.position)} />
+              )}
+            </Level>
+          ))}
       </div>
     </div>
   );
