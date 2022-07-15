4b969edde8b6760dc12e7d81a5cf54c1a388b2e3
Kazerlelutin <b@bouteiller.contact>
2022-07-16 00:39:07 +0200

    fix(og-balise): fix matomo code

--- components/OgBalise/OgBalise.tsx
+++ components/OgBalise/OgBalise.tsx
@@ -16,22 +16,21 @@ export default function OgBalise({ img, description, title }: props) {
   function matomo() {
     return {
       __html: `
-          <!-- Matomo -->
-          <script>
-            var _paq = window._paq = window._paq || [];
-            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
-            _paq.push(['trackPageView']);
-            _paq.push(['enableLinkTracking']);
-            (function() {
-              var u="//analytics.bouteiller.contact/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '5']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
-            })();
-          </script>
-          <!-- End Matomo Code -->
-        `,
+      //<![CDATA[
+        var _paq = window._paq = window._paq || [];
+        /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+        _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+        _paq.push(['trackPageView']);
+        _paq.push(['enableLinkTracking']);
+        (function() {
+          var u="//analytics.bouteiller.contact/";
+          _paq.push(['setTrackerUrl', u+'matomo.php']);
+          _paq.push(['setSiteId', '5']);
+          var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+          g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+        })();
+        //]]>
+    `,
     };
   }
 
